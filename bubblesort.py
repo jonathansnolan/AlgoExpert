0baa621b6ba321aed2ee7cd6e5a4fc5fc45bbdad
Jonathan-Nolan <jonathan.nolan.jn@gmail.com>
2021-07-19 19:48:51 +0100

    Initial Commit

---
+++ bubblesort.py
@@ -0,0 +1,13 @@
+def BubbleSort(array):
+    swap = "yes"
+    while swap == "yes":
+        swap = "no"
+        for k in range(len(array)-1):
+            if array[k] > array[k+1]:
+                array[k], array[k+1] = array[k+1], array[k]
+                print(array)
+                swap = "yes"
+    print("HERE IS FINAL ANSWER\n")
+    print(array)
+
+print(BubbleSort([4,3,2,1,6,4,5,7,8]))
